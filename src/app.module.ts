15bb1c623b77bb1ad0ecc6f26d821c164536c3a5
Michael Paulinus <michaelpaulinus@gmail.com>
2025-08-24 23:54:20 +0200

    Removed weather module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WeatherModule } from './weather/weather.module';
 import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [WeatherModule, FirebaseModule],
+  imports: [FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/weather/weather.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WeatherService } from './weather.service';
-import { WeatherController } from './weather.controller';
-
-@Module({
-  providers: [WeatherService],
-  controllers: [WeatherController],
-})
-export class WeatherModule {}
