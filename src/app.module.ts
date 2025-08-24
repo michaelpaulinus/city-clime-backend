6801b83ee0c457599948ca7b24d2797237d4cfc4
Michael Paulinus <michaelpaulinus@gmail.com>
2025-08-25 00:44:25 +0200

    Imported all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
+import { ExternalWeatherApiModule } from './external-weather-api/external-weather-api.module';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [FirebaseModule, ExternalWeatherApiModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
