fcd7456e12469dcc193c69b43e8bbfd23e422d02
Michael Paulinus <michaelpaulinus@gmail.com>
2025-08-25 01:34:14 +0200

    Returned response data

--- src/external-weather-api/external-weather-api.service.ts
+++ src/external-weather-api/external-weather-api.service.ts
@@ -6,16 +6,33 @@ import ForecastWeather from 'src/common/dtos/forecast-weather';
 @Injectable()
 export class ExternalWeatherApiService {
   private EXTERNAL_WEATHER_API_BASE_URL = process.env.EXTERNAL_WEATHER_API_BASE_URL;
+  private EXTERNAL_WEATHER_API_KEY = process.env.EXTERNAL_WEATHER_API_KEY;
 
   async getCurrentWeather(location: string) {
-    return axios.get<CurrentWeather>(
-      `${this.EXTERNAL_WEATHER_API_BASE_URL}/api/weather/current/${location}`
+    const response = await axios.get<CurrentWeather>(
+      `${this.EXTERNAL_WEATHER_API_BASE_URL}/current.json`,
+      {
+        params: {
+          q: location,
+          key: this.EXTERNAL_WEATHER_API_KEY
+        }
+      }
     );
+
+    return response.data;
   }
 
   async getForecastedWeather(location: string) {
-    return axios.get<ForecastWeather>(
-      `${this.EXTERNAL_WEATHER_API_BASE_URL}/api/weather/forecast/${location}`
+    const response = await axios.get<ForecastWeather>(
+      `${this.EXTERNAL_WEATHER_API_BASE_URL}/forecast.json`,
+      {
+        params: {
+          q: location,
+          key: this.EXTERNAL_WEATHER_API_KEY
+        }
+      }
     );
+
+    return response.data;
   }
 }
