2aca68a7e94deab18ed794b81077076bb4bb671c
Michael Paulinus <michaelpaulinus@gmail.com>
2025-08-25 01:35:01 +0200

    Changed position of GET method

--- src/firebase/firebase.controller.ts
+++ src/firebase/firebase.controller.ts
@@ -4,13 +4,13 @@ import { FirebaseService } from './firebase.service';
 export class FirebaseController {
   constructor(private readonly firebaseService: FirebaseService) {}
 
-  @Put('/forecasted-weather/:location')
-  async addForecastedWeather(@Param('location') location: string) {
-    await this.firebaseService.addForecastedWeather(location);
-  }
-
   @Get('/forecasted-weather')
   async getForecastedWeather() {
     return await this.firebaseService.getForecastedWeather();
+  }
+
+  @Put('/forecasted-weather/:location')
+  async addForecastedWeather(@Param('location') location: string) {
+    await this.firebaseService.addForecastedWeather(location);
   }
 }
