21d220592be73db75c3af070bd930fbe1b3eb70a
Michael Paulinus <michaelpaulinus@gmail.com>
2025-08-25 00:09:25 +0200

    Added common dtos

---
+++ src/common/dtos/forecast-weather.ts
@@ -0,0 +1,11 @@
+import Location from './location';
+import Current from './current';
+import ForecastDay from './forecast-day';
+
+export default interface ForecastWeather {
+  location: Location;
+  current: Current;
+  forecast: {
+    forecastday: ForecastDay[];
+  };
+}
